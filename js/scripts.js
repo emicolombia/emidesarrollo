1e60df7d1aa9f3deb21f47362cd2b6f16f2d732e
victor <vmlp1996@gmail.com>
2022-11-06 22:24:34 -0500

    actualizacion 2

--- js/scripts.js
+++ js/scripts.js
@@ -44,4 +44,48 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-});
+});     
+
+    //instagram//
+    'use strict'
+    
+    const galery = document.querySelector('.galery');
+    const feed = document.querySelector('.contenedor-galery');
+    const next = document.querySelector('#next');
+    const prev = document.querySelector('#prev');
+    const fields = "id,media_type,media_url,thumbnail_url,timestamp,permalink,caption";
+    const limit = 10;
+
+    const token = 'EAAHZAZBqwg3isBAIV6ZCych5aYCZAozYXULUAnGs1o9MULBAAX5TZB43mXcylmpErcpBh7ZCmgY59H9eroOihx6uokyVvmu2oZAPwn0qdEWqpshkXRRMigxd7sy2tn1YZCB0RADJvxFkEam2nJTmjZAGpYh7Ve6H7PCwilDAflZBN9Pa69KZAZBNI8vTqwwAyiCggJMEF5rlX0kWhCjqxHkFAZBpR';
+    const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${token}&limit=${limit}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => CrearHtml(data.data))
+
+    function CrearHtml(data){
+        for (const img of data){
+            galery.innerHTML += `
+            <div class="image overflow">
+            <img loading="lazy" src="${img.media_url }" alt="">
+            <div class="opacity-hover">
+                <a href="${img.permalink}" class="caption">
+                    <p>
+                    ${img.caption.slice(0, 100)}
+                    </p>
+                </a>
+            </div>
+            </div>
+            `
+        }
+    }
+next.addEventListener('click', moveGalery);
+prev.addEventListener('click', moveGalery);
+
+function moveGalery(e){
+    if(e.target.id === 'next' || e.target.parentElement.id === 'next'){
+        feed.scrollLeft += feed.offsetWidth;
+    }else{
+        feed.scrollLeft -= feed.offsetWidth;
+    }
+}
